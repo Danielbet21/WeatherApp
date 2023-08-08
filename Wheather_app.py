eb12e3f36037b964f9763cf592a86ff1d9f4363f
Daniel <danielbetzalel16@gmail.com>
2023-08-08 10:01:56 +0300

    Final results by adding graph

--- Wheather_app.py
+++ Wheather_app.py
@@ -17,14 +17,14 @@ def get_forcast():
     global img1
     base = Toplevel()
     base.title("Global weather app")
-    base.geometry("400x500")
+    base.geometry("400x600")
     
 #-------------------------------------------------------------------------------------------------------------------------------
 
     location =entry.get()
     location =location.replace(" ","+")
     
-    api_req = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key=5580229a7d3042049da132814232807&q={location}&days=1&aqi=no&alerts=no")
+    api_req = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key=5580229a7d3042049da132814232807&q={location}&days=5&aqi=no&alerts=no")
 
     try:
 
@@ -36,7 +36,7 @@ def get_forcast():
         chance_of_rain =api['forecast']['forecastday'][0]['day']['daily_chance_of_rain']
         
         #appending all the min,max values 
-        for forcast_day in api["forecastday"]:
+        for forcast_day in api['forecast']["forecastday"]:
             day = forcast_day["day"]
             maxDay.append(day["maxtemp_c"])
             minDay.append(day["mintemp_c"])
@@ -46,7 +46,7 @@ def get_forcast():
 
     #-------------------------------------------------------------------------------------------------------------------------------
     # the upper area - today forcast: temp.wind,rain 
-    color ="light blue"
+    color = "#aee0f5"
     #Create an frame
     today_frame= Frame(base, bg= color,bd=20)
 
@@ -82,26 +82,37 @@ def get_forcast():
     
     today_frame.pack(expand=True,fill=BOTH)
 
-# today = date.today()
-# num_of_day = today.strftime("%d")
 #-------------------------------------------------------------------------------------------------------------------------------
- #The bars frame: 7 days forcast base on max & min temp
-    fig ,ax = plt.subplots()
-    ax.plot(minDay,maxDay)
+#The bars frame: 7 days forcast base on max & min temp
     
-    #Create an frame
+    fig ,ax = plt.subplots() 
+    
     bar_frame= Frame(base, bg= color,bd=20)
-    #Create canvas to "draw" in it the matplotlib fig
-    canvas = FigureCanvasTkAgg(fig, master=bar_frame)
+    
+    canvas =FigureCanvasTkAgg(fig, master=bar_frame)
+    canvas.get_tk_widget().pack()
+    
+    days =['sun','mon','tue','wed','thu']
+    
+    plt.style.use('fivethirtyeight')
+    fig.patch.set_facecolor(color)
+    
+    ax.set_title("Temp")
+    ax.plot(days,maxDay,label="max",color="#f54242",linewidth=2)
+    ax.plot(days,minDay,label="min",color="#7bcbed",linewidth=2)
+    
+    plt.legend(loc="best",fontsize=10)
+    ax.grid(True)
+    plt.tight_layout()
+
     canvas.draw()
-    canvas.get_tk_widget().pack
     
     bar_frame.pack(expand=True,fill=BOTH)
 #-------------------------------------------------------------------------------------------------------------------------------
 #Opening page
-open_frame= Frame(start, bg= "light blue",bd=20)
+open_frame= Frame(start, bg= "#aee0f5",bd=20)
 
-lab = Label(open_frame,text="type your desire destanation",bg="light blue")
+lab = Label(open_frame,text="type your desire destanation",bg="#aee0f5")
 lab.config(font=("Fixedsys",10))
 
 entry = Entry(open_frame)
